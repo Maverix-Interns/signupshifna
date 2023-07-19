d0aea78fffd793787f5accade732b958637c3a30
shifna-usman <shifnausman14@gmail.com>
2023-07-19 23:03:02 +0530

    jj

--- src/app/createaccount/createaccount.component.ts
+++ src/app/createaccount/createaccount.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from '../auth.service';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../../envs/envs';
 @Component({
   selector: 'app-createaccount',
   templateUrl: './createaccount.component.html',
---
+++ src/envs/envs.ts
@@ -0,0 +1,13 @@
+export const environment = 
+{
+production: false,
+firebase: {
+  apiKey: "AIzaSyDidLJjH9d8DT1JAu-enFo6ff3mtp-OvHM",
+  authDomain: "taskmanagerappmvx.firebaseapp.com",
+  projectId: "taskmanagerappmvx",
+  storageBucket: "taskmanagerappmvx.appspot.com",
+  messagingSenderId: "215030935704",
+  appId: "1:215030935704:web:1774121e69457665b620ed",
+  measurementId: "G-HXMLP21PYC"	
+ 	}
+};
